9b9cadecab25946f1a70610de24c9bb99c02beea
dmetagame <118340345+dmetagame@users.noreply.github.com>
2026-08-19 16:57:10 +0100

    Normalize Solidity interface formatting

--- src/interfaces/IUniswapV3.sol
+++ src/interfaces/IUniswapV3.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.26;
 interface IUniswapV3Factory {
     function getPool(address tokenA, address tokenB, uint24 fee) external view returns (address pool);
 }
+
 interface IUniswapV3Pool {
     function factory() external view returns (address);
     function token0() external view returns (address);
